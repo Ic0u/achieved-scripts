e0c30c81c811c35270d665aa26c3fe009e7a4ce2
Nguyễn Nam <soniclolgaming@gmail.com>
2022-03-02 09:55:42 +0700

    Update cresentbackup.lua

--- cresentbackup.lua
+++ cresentbackup.lua
@@ -1,2 +1,7 @@
-https://raw.githubusercontent.com/Ic0u/-Cresent-Hub/main/CresentMain
+--[[   
+                                                         Created by Ic0u !
+                                                  Brought you by Cresent Client !
+                                                 subcribe to Actuallyblox in yt !
+--]]
+
 loadstring(game:HttpGet('https://raw.githubusercontent.com/Ic0u/-Cresent-Hub/main/CresentMain', true))()
