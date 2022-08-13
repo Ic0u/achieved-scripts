9e4c7ccdb8e269c44fe783893a550e51502cf1b0
Nguyễn Nam <soniclolgaming@gmail.com>
2022-08-13 07:54:22 +0700

    Update autofarmuilib.lua
    So

--- autofarmuilib.lua
+++ autofarmuilib.lua
@@ -171,9 +171,9 @@ function DarkLib:Window()
 	Title.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 	Title.BackgroundTransparency = 1.000
 	Title.Position = UDim2.new(0.42778793, 0, 0.041837737, 0)
-	Title.Size = UDim2.new(0, 78, 0, 30)
+	Title.Size = UDim2.new(0, 78, 0, 30
 	Title.Font = Enum.Font.Gotham
-	Title.Text = "Dark Hub"
+	Title.Text = "Sơn Béo Hub"
 	Title.TextColor3 = Color3.fromRGB(168, 168, 168)
 	Title.TextSize = 20.000
 
